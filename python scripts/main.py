4ab49fd80cfcdadda0262fdfecd229145b352ca6
YeohJK <spin8053@gmail.com>
2025-10-22 16:16:54 +0800

    Modified open file path to test script logic.

--- python scripts/main.py
+++ python scripts/main.py
@@ -4,7 +4,7 @@
 import csv
 
 # Postcondition: Opens file of interest and extracts azimuth value at maximum elevation
-with open("ContactLocator_TLE_SAT_2.txt", "r") as file_obj:
+with open("data/ContactLocator_TLE_SAT_2.txt", "r") as file_obj:
     csv_obj = csv.reader(file_obj)
     print("Start")
     # Postcondition: Extracts azimuth value at maximum elevation
