17608cfabbd55f0b792751adf83c67d1d5d72328
YeohJK <spin8053@gmail.com>
2025-10-28 20:01:52 +0800

    Modified script to use readline method rather than csv reader. Resolved bug of not extracting information for last pass.

--- python scripts/main.py
+++ python scripts/main.py
@@ -1,50 +1,58 @@
 # Main script for data transformation
 
-# Postcondition: Imports required modules/packages
-import csv
-
 # Creates list of pass information
 list_passes = []
 
 # Postcondition: Opens file of interest and extracts azimuth value at maximum elevation
-with open("data/ContactLocator_TLE_SAT_2.txt", "r") as file_obj:
-    csv_obj = csv.reader(file_obj)
+with open("data/ContactLocator_TLE_SAT_2.txt", "r") as file:
     print("Starting parsing\n")
-    # Loops through rows of txt file
-    row_ind = 0
+    # Prints first row of text file
+    row_str = file.readline()
+    row = row_str[:-1]
+    print(row)
+    # Establishes variables
+    row_ind = 1
     ele_val_max = 0.0
-    for row in csv_obj:
+    # Loops through rows of txt file
+    while row_str.endswith("\n"):
+        # Extracts and prints content from row
+        row_prev_str = row_str
+        row_str = file.readline()
+        row = row_str[:-1]
+        print(row)
         # Ensures parsing starts on row with index of 4 which is the first row of data
         if row_ind < 5:
             # Stores information for first pass
             if row_ind == 4:
-                pass_num_current = int(row[0][0:11])
-                ele_val_max = float(row[0][76:91])
-                azi_val = float(row[0][58:71])
-                dt_sta_val = row[0][29:53]
+                pass_num = int(row[0:11])
+                ele_val_max = float(row[76:91])
+                azi_val = float(row[58:71])
+                dt_sta_val = row[29:53]
             row_ind += 1
             continue
-        # Skips empty rows dividing passes
-        elif row == []:
-            continue
-        # Continues to next loop when reached new pass
-        pass_num = int(row[0][0:11])
-        if pass_num > pass_num_current:
+        # For case of row at EOF or 1st row of new pass as previous row was a empty divider row   
+        elif row_prev_str == "\n":
             # Stores information for prev pass
             dt_end_val = dt_val
             list_passes.append([dt_sta_val, dt_end_val, azi_val])
+            # Breaks loop when EOF is reached
+            if row_str == "":
+                break
             # Stores information for new pass
-            pass_num_current = pass_num
-            ele_val_max = float(row[0][76:91])
-            azi_val = float(row[0][58:71])
-            dt_sta_val = row[0][29:53]
+            pass_num = int(row[0:11])
+            ele_val_max = float(row[76:91])
+            azi_val = float(row[58:71])
+            dt_sta_val = row[29:53]
             continue
-        # Checks elevation angle with that of previous value
-        ele_val = float(row[0][76:91])
-        dt_val = row[0][29:53]
+        # Skips empty divider rows
+        elif row == "":
+            continue
+        # For 2nd row of a pass onwards, checks elevation angle with that of previous value
+        ele_val = float(row[76:91])
+        dt_val = row[29:53]
+        # Stores information on current row if max elevation thus far is reached in pass
         if ele_val > ele_val_max:
             ele_val_max = ele_val
-            azi_val = float(row[0][58:71])
-        print(row)
+            azi_val = float(row[58:71])
 print(list_passes)
 print(len(list_passes))
